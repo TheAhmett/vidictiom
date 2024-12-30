33a04bc8ce9e3eeed9bc10a636542d7f89d5efef
TheAhmett <atakhelikopteri3446@gmail.com>
2024-12-30 20:44:27 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const hedefDizi = ["a","h","m","e","t"]; // Hedef tuş sırası
+let basilanlar = []; // Basılan tuşları takip etmek için boş dizi
+
+document.addEventListener("keydown", function(event) {
+    basilanlar.push(event.key); // Basılan tuşu diziye ekle
+
+    // Eğer basılanların sayısı hedef diziyi geçerse eski tuşları sil
+    if (basilanlar.length > hedefDizi.length) {
+        basilanlar.shift(); // İlk elemanı kaldır
+    }
+
+    // Basılan tuş dizisi hedef diziyi tutuyorsa işlem yap
+    if (JSON.stringify(basilanlar) === JSON.stringify(hedefDizi)) {
+        window.location.href = "https://raw.githubusercontent.com/TheAhmett/cemal/refs/heads/main/cem.jpg", "_blank"
+        basilanlar = []; // İşlemden sonra basılanları sıfırla
+    }
+})
