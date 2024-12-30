e2d1735e839f3f027b20c221e82436ab62058b8e
TheAhmett <atakhelikopteri3446@gmail.com>
2024-12-30 20:45:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-const hedefDizi = ["a","h","m","e","t"]; // Hedef tuş sırası
-let basilanlar = []; // Basılan tuşları takip etmek için boş dizi
+const hedefDizi = ["a","h","m","e","t"]
+let basilanlar = []
 
 document.addEventListener("keydown", function(event) {
-    basilanlar.push(event.key); // Basılan tuşu diziye ekle
+    basilanlar.push(event.key)
 
-    // Eğer basılanların sayısı hedef diziyi geçerse eski tuşları sil
     if (basilanlar.length > hedefDizi.length) {
-        basilanlar.shift(); // İlk elemanı kaldır
+        basilanlar.shift()
     }
 
-    // Basılan tuş dizisi hedef diziyi tutuyorsa işlem yap
     if (JSON.stringify(basilanlar) === JSON.stringify(hedefDizi)) {
         window.location.href = "https://github.com/TheAhmett/vidictiom/blob/main/ahmet.png?raw=true", "_blank"
-        basilanlar = []; // İşlemden sonra basılanları sıfırla
+        basilanlar = []
     }
 })
